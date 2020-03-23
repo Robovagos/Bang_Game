2b43da33c2a26c8caaf4348addf807bed7565167
Deoll <Deoll@VAGELIS-PC>
2020-03-23 04:57:48 +0200

    Just for testing

--- src/bang/Bang.java
+++ src/bang/Bang.java
@@ -5,6 +5,9 @@
  */
 package bang;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Γιωργος
@@ -16,7 +19,32 @@ public class Bang {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Hero myHero = new Hero("Vagelis");
+        //Hero myHero = new Hero("Vagelis");
+        List<Player> playersList = new ArrayList<>();
+        Player newPlayer = new Sheriff("Deoll");
+        Player newPlayer2 = new Sheriff("Varch");
+        Player newPlayer3 = new Sheriff("Nickel");
+        Player newPlayer4 = new Sheriff("Raptis");
+        
+        playersList.add(newPlayer);
+        playersList.add(newPlayer2);
+        playersList.add(newPlayer3);
+        playersList.add(newPlayer4);
+        //(Player)playersList.get(0).arrowAttack(); For Testing
+        
+        /*newPlayer.arrowAttack();
+        Dice A = new Arrow(newPlayer, playersList);
+        Dice B = new Arrow(newPlayer, playersList); 
+        Dice c = new Arrow(newPlayer2, playersList);
+        Dice d = new Arrow(newPlayer, playersList);
+        Dice e = new Arrow(newPlayer3, playersList);
+        Dice f = new Arrow(newPlayer4, playersList); 
+        Dice g = new Arrow(newPlayer4, playersList);
+        Dice h = new Arrow(newPlayer2, playersList);
+        Dice i = new Arrow(newPlayer3, playersList);
+        Dice q = new Arrow(newPlayer, playersList);        
+        System.out.println(newPlayer.getNumberOfArrows());*/
+        
     }
     
 }
